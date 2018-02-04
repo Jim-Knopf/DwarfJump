e7722f8a3c42b4aeade091670166a930cf38bc0f
JohnnyRieder <JohnnyRieder@users.noreply.github.com>
2018-02-04 16:29:19 +0100

    Ich

--- app/src/main/java/my/company/dwarfjump/Home.java
+++ app/src/main/java/my/company/dwarfjump/Home.java
@@ -1,18 +1,32 @@
 package my.company.dwarfjump;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Button score=(Button)findViewById(R.id.scorebutton);
+        score.setOnClickListener(this);
         }
 
+
+    @Override
+    public void onClick(View view) {
+
+        Intent scoreboard = new Intent(Home.this, scoreboard.class);
+
+        startActivity(scoreboard);
+
+    }
 }
