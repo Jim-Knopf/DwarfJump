95154b01416902dd9e11a6dad347d26cb8c7e6c2
JohnnyRieder <JohnnyRieder@users.noreply.github.com>
2018-02-04 16:08:54 +0100

    Wer ist der coolste hier

---
+++ app/src/main/java/my/company/dwarfjump/Home.java
@@ -0,0 +1,18 @@
+package my.company.dwarfjump;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        }
+
+}
