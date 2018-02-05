e195b8118a12878a770a0e57a868d58645b558cf
JohnnyRieder <JohnnyRieder@users.noreply.github.com>
2018-02-05 07:06:30 +0100

    Changed about us file and created new header names

--- app/src/main/java/my/company/dwarfjump/AboutUs.java
+++ app/src/main/java/my/company/dwarfjump/AboutUs.java
@@ -6,11 +6,12 @@ import android.widget.TextView;
 
 public class AboutUs extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-
 
     }
 }
