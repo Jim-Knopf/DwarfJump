e84b4492952059b55058c49526217d2dff9e1c5d
JohnnyRieder <JohnnyRieder@users.noreply.github.com>
2018-02-06 23:57:57 +0100

    The dwarf is able to drink now

--- app/src/main/java/my/company/dwarfjump/AboutUs.java
+++ app/src/main/java/my/company/dwarfjump/AboutUs.java
@@ -1,17 +1,66 @@
 package my.company.dwarfjump;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class AboutUs extends AppCompatActivity {
+public class AboutUs extends AppCompatActivity implements View.OnTouchListener{
 
+    Button b1;
+    RelativeLayout r1;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+        b1 = (Button) findViewById(R.id.drinklittledwarf);
+        r1 = (RelativeLayout) findViewById(R.id.aboutus);
+
+        b1.setOnTouchListener(new View.OnTouchListener(){
+
+
+
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        r1.setBackgroundResource(R.drawable.drinkindwarf);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        r1.setBackgroundResource(R.drawable.drunkendwarf_withbackground);
+                        break;
+
+
+                }
+
+
+
+
+
+                return false;
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
     }
 }
+
+
+
